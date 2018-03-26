ae2f71f3924d16f8bd4e6062b3c587f1d63c642f
mak66 <mak66@users.noreply.github.com>
2018-03-26 16:08:15 +0100

    Initial Load

---
+++ src/index.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square(props) {
+    return (
+      <button
+        className="square"
+        onClick={props.onClick}
+        style={{backgroundColor: (props.onWinLine ? 'red' : 'white')}}>
+        {props.value}
+      </button>
+    );
+}
+
+class Board extends React.Component {
+  renderSquare(i,winLine) {
+    return <Square
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
+      onWinLine={winLine.includes(i)}
+           />;
+  }
+  
+  renderRow(r,winLine){
+    return (
+      <div className="board-row">
+        {this.renderSquare(r*3,winLine)}
+        {this.renderSquare(r*3+1,winLine)}
+        {this.renderSquare(r*3+2,winLine)}
+      </div>
+
+    )
+  }
+
+
+  render() {
+    const winLine = winningLine(this.props.squares);
+    return (
+      <div>
+        {this.renderRow(0,winLine)}
+        {this.renderRow(1,winLine)}
+        {this.renderRow(2,winLine)}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history:[{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+      stepNumber: 0,
+    }
+  }
+  handleClick(i){
+    const history = this.state.history.slice(0,this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+      return (
+        <li key={move}>
+          <button className="listButton" onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' +winner;
+    } else if (this.state.stepNumber === 9) {
+      status = 'Draw';
+    } else {
+      status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div>
+        <div className="game">
+          <div className="game-board">
+            <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
+          </div>
+          <div className="game-info">
+            <div>History</div>
+            <ol>{moves}</ol>
+          </div>
+        </div>
+        <div className="status">
+          <div>{status}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+function winningLine(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return lines[i];
+    }
+  }
+  return [];
+}
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
